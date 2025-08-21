faee788331c63553cc66721f995d0fd60a49c2c4
Arka <arkapravasantra17@gmail.com>
2025-08-21 12:37:41 +0530

    socket.io

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,9 +1,35 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer);
+
+  io.use(async (socket, next) => {
+    try {
+      const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+      if (!cookies.token) {
+        return next(new Error("Authentication error"));
+      }
+
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+
+      if (!user) {
+        return next(new Error("User not found"));
+      }
+
+      socket.user = user;
+      next();
+    } catch (error) {
+      next(new Error("Invalid token"));
+    }
+  });
+
   io.on("connection", (socket) => {
-    console.log("New socket connection:", socket.id);
+    console.log("user connected", socket.user)
+    console.log("New socket connection:", socket.id, "User:", socket.user?.email);
   });
 }
 
